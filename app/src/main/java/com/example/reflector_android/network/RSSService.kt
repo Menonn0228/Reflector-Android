10143aedddb53ceb2efde9f1d34dd04676583276
Caleb Morgan <56972103+ExoticWifi@users.noreply.github.com>
2020-09-28 19:55:04 -0500

    Feature/infinite scroll (#12)
    * Infinite Scrolling
    
    Added infinite scrolling feature
    
    * Code and progress bar revision
    
    Did the code change request and changed how the progress bar loads in
    
    Co-authored-by: Caleb <ctm365@gmail.com>

--- app/src/main/java/com/example/reflector_android/network/RSSService.kt
+++ app/src/main/java/com/example/reflector_android/network/RSSService.kt
@@ -6,7 +6,28 @@ import java.io.IOException
 
 class RSSService {
     suspend fun fetchNews(): MutableList<Article>? {
-        val url = "http://reflector-online.com/search/?f=rss&t=article&s=start_time&sd=desc&l=50&c=news/*"
+        val url = "http://reflector-online.com/search/?f=rss&t=article&s=start_time&sd=desc&l=20&c=news/*"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        var test = XmlParser()
+        var articles = mutableListOf<Article>()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) {
+                throw IOException("Unexpected code $response")
+            }
+
+            if (response.body == null) {
+                return null
+            }
+            val body = response.body!!.byteStream()
+            articles = test.parse(body)
+        }
+        return articles
+    }
+
+    suspend fun fetchMoreNews(offset: Int): MutableList<Article>? {
+        val url = "http://reflector-online.com/search/?f=rss&t=article&s=start_time&sd=desc&l=20&o=$offset&c=news/*"
         val request = Request.Builder().url(url).build()
         val client = OkHttpClient()
         var test = XmlParser()
